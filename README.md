3c3b7807a7172fe5f94331de05513e0506ba0f73
KRN <{3194336}+{0xKRN}@users.noreply.github.com>
2022-08-29 08:35:06 +0200

    Tidy up. Typos.

--- README.md
+++ README.md
@@ -7,31 +7,32 @@ We approached this like a beginner would so that it's easier to digest and creat
 It's designed for those of you that are interested in learning programming and think it would be cool to host your own bot. 
 
 ### How does it work?  
-The bot pulls data from [Coingecko's API](https://www.coingecko.com/en/api) and pushes the results to the bots nickname. 
+The bot pulls data from [Coingecko's API](https://www.coingecko.com/en/api) and pushes the results to the bots nickname. The bot updates every 60s which can be changed in the `config.json`.
 
 ### Checklist before starting
 1. [Install Node & Npm](https://nodejs.org/en/download/)  
 2. [Install Visual Studio Code](https://code.visualstudio.com/) / Other code editor.
 3. [Install Git](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git)
-4. [Fork the Repo](https://github.com/EternullCode/Free-Coingecko-Discord-Bot/fork) (If you need help [click here](https://docs.github.com/en/get-started/quickstart/fork-a-repo#forking-a-repository))
+4. [Fork the Repo](https://github.com/EternullCode/Free-Coingecko-Discord-Bot/fork)
 
 ### Creating a Discord application
 [Follow these steps on our Notion](https://eternull.notion.site/Eternull-s-Free-Coingecko-Bot-Setup-dd9ef5765a1845e4959152033c2299d1)
 
 
 ### Running the bot
-1. Open your code editor.
-2. Open the terminal. In VSC the standard keybind is `` CTRL + ` ``. Alternatively you can click on `Terminal -> New Terminal` in the toolbar.
-3. Clone the forked repo in your code editor. To do this follow [this](https://docs.github.com/en/get-started/quickstart/fork-a-repo#cloning-your-forked-repository) guide.
-4. Once you see the repo files in your code editor, type `npm install` and hit enter. This will load the necessary dependencies.
+1. Open your code editor
+2. Open the terminal. In VSC the standard keybind is `` CTRL + ` ``. Alternatively you can click on `Terminal -> New Terminal` in the toolbar
+3. Clone the forked repo in your code editor. To do this follow [this](https://docs.github.com/en/get-started/quickstart/fork-a-repo#cloning-your-forked-repository) guide
+4. Once you see the repo files in your code editor, type `npm install` and hit enter. This will load the necessary dependencies
 5. Put your bot token ID in the `.env` file and press `CTRL + S` to save. It should look something like this when complete: `DISCORD_TOKEN_ID=myKeyGoesHere`
 6. Check `config.json` and enter the desired `coingeckoId`. Examples: `ethereum`, `bitcoin`, `usd-coin`
 7. Type `node index.js` in your terminal and hit enter. Your bot should now come online! 🎉
 
 ### Where to find the Coingecko API ID?
-Go to the [Coingecko](https://www.coingecko.com/) website  
-The API ID can be found on each ticker's page.  
-![](./assets/images/coingeckoApiExample.png)
+1. Go to the [Coingecko](https://www.coingecko.com/) website  
+2. Choose a coin. We'll use Bitcoin (BTC) as an example
+3. The `API id` can be found on the info section. This is what you're pasting into the `config.json`
+![](https://eternull.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F4b7f1af2-dfc7-4b5f-a15e-eb6cabeeb631%2FcoingeckoApi.png?table=block&id=9e887a4b-4ae8-4515-93e9-0d4cc406a4a2&spaceId=392aea56-6ad3-4992-9120-0deee1ce1741&width=2000&userId=&cache=v2)
 
 ### Issues?
 Join our [Discord](https://discord.gg/UyK2mfxgcB) for assistance.
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Read about 'use-strict' here: https://www.w3schools.com/js/js_strict.asp
 "use strict"
 
-// Import and configure dotenv so we can store our Discord 'Token ID' as an environmental variable: https://www.npmjs.com/package/dotenv
-// A Discord Token ID is obtained from Discords Dev Portal: https://discord.com/developers/applications
+// Import and configure dotenv so we can store our Discord 'token ID' as an environmental variable: https://www.npmjs.com/package/dotenv
 require('dotenv').config();
 
 // Import the node-fetch library. We're using this later to fetch data from CoinGecko's API.
@@ -12,73 +11,77 @@ const fetch = require('node-fetch');
 // We need these to initialise the bot and retrieve the correct information from guilds.
 const { Client, Intents } = require('discord.js');
 
-// Declaring the bot as 'bot' and assigning the correct intents.
+// Declaring the bot as and assigning the correct intents.
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // Importing the values from the 'config.json' file.
-const { coingeckoId, ticker, customStatus, updateInterval } = require('./config.json');
+const { APIid, ticker, customStatus, updateInterval } = require('./config.json');
 
 // Declaring the main function.
 function getCoingecko() {
 
   // Declaring the guildMeCache array. We're using this to store all guilds the bot is a
-  // member of. Read about arrays here: https://www.w3schools.com/js/js_arrays.asp
+  // currently a member of. Read about arrays here: https://www.w3schools.com/js/js_arrays.asp
   let guildMeCache = [];
 
-  // Check if user has filled in .env & config.json files. Won't run otherwise.
-  if (process.env.DISCORD_TOKEN_ID != "" && coingeckoId != "") {
+  // Check if user has filled in the .env & config.json files. Won't run otherwise.
+  if (process.env.DISCORD_TOKEN_ID != "" && APIid != "") {
 
-    // Ready up activities. Pushing all current guilds the bot is in to the guildMeCache Array.
+    // Ready up activities. Pushing all guilds the bot is currently a member of to the 'guildMeCache' Array.
     bot.on("ready", () => {
       bot.guilds.cache.each((guild) => guildMeCache.push(guild));
 
       // Login activity. Example: 'Logged in as fluffycow#1234'. 
       console.log(`Logged in as ${bot.user.tag}!`);
 
-      // Check for custom status. Default to 'Watching Discord.gg/Eternull if empty'
+      // Check for custom status. Default to "Watching Discord.gg/Eternull if empty". It's free real-estate.
       if (customStatus != "") {
         bot.user.setActivity(customStatus, { type: "WATCHING" });
       } else {
-        bot.user.setActivity(`Discord.gg/Eternull`, { type: "WATCHING" }); // It's free real-estate.
+        bot.user.setActivity(`Discord.gg/Eternull`, { type: "WATCHING" });
       }
 
-      // Check for custom update interval. Else set it to 60s. This is to avoid IP bans from Discord.
-      // They're usually 30mins so don't worry if it happens.
+      // Check if 'update interval' is empty or less than 60s. This will set it to 60s to avoid rate limits.
+      // Abusing rate-limits can result in an IP ban from Discord. It's usually 30mins so don't worry if it happens.
+      // Read about rate-limits here: https://discord.com/developers/docs/topics/rate-limits
       if (updateInterval === "" || updateInterval < 60000) {
         updateInterval = 60000;
         console.log("updateInterval(config.json) missing or less than 60000! Setting to 60000 to avoid Discord API rate-limits.");
       }
 
-      // Run the asynchronous fetchCoingeckoData function.
+      // Run the asynchronous 'fetchCoingeckoData' function.
       // Read more about asynchronous functions here: https://www.w3schools.com/js/js_async.asp
       fetchCoingeckoData()
 
-      // This will run the fetchCoingeckoData function every 60+ seconds and retrieve new price data.
+      // This will run the 'fetchCoingeckoData' function every 60+ seconds.
       setInterval(fetchCoingeckoData, updateInterval);
     })
+
   } else {
-    console.log("TIP: DISCORD_TOKEN_ID(.env file) or coingeckoId(config.json file) missing.");
+    console.log("TIP: DISCORD_TOKEN_ID(.env file) or APIid(config.json file) missing.");
   }
 
   async function fetchCoingeckoData(price) {
 
+    // Here we're using a try / catch to catch the error and output our own custom error.
+    // Read about try catch here: https://www.w3schools.com/js/js_errors.asp
     try {
 
-      // Fetch data from the CG v3 API and parses the response to JSON.
+      // Fetch data from the CG v3 API and parses the JSON response to a JavaScript object.
       // Read about Response.json() here: https://developer.mozilla.org/en-US/docs/Web/API/Response/json
-      const coingeckoData = await fetch(`https://api.coingecko.com/api/v3/coins/${coingeckoId}`)
+      const coingeckoData = await fetch(`https://api.coingecko.com/api/v3/coins/${APIid}`)
         .then(response => response.json());
 
         //Pull in the price as USD & saves it to the variable 'price'.
         price = coingeckoData.market_data.current_price.usd;
       } catch (error) {
-        console.log("TIP: No data is being pulled in for the coingeckoId provided. Check config.json for errors.")
+        console.log("TIP: No data is being pulled in for the APIid provided. Check config.json for errors.")
 
-        // Remove '//'below if you want to see the REAL errors.
+        // Remove '//' below if you want to see the REAL errors.
         // console.log(error)
       }
 
-      // Check to see if the data has returned a value. Run the setBot function and pass through the price.
+      // Check to see if the data has returned a value. Run the setBot function and pass through the price variable.
       if (!isNaN(price) && price != null) {
         setBot(price)
       }
@@ -86,18 +89,15 @@ function getCoingecko() {
   }
 
   function setBot(price) {
-    for (let i = 0; i < guildMeCache.length; i++) {
 
-      // Check if ticker(config.json) has a value. Otherwise run the else statement.
-      if (ticker != "") {
-        guildMeCache[i].me.setNickname(`${ticker}$${price.toLocaleString("en-US")}`);
-      } else {
-        guildMeCache[i].me.setNickname(`$${price.toLocaleString("en-US")}`);
-      }
+    // Here we're using a 'for loop' to iterate through each guild in the 'guildMeCache' and push the price variable
+    // as the bot's nickname. Read about for loops here: https://www.w3schools.com/js/js_loop_for.asp
+    for (let i = 0; i < guildMeCache.length; i++) {
+        guildMeCache[i].me.setNickname(`${ticker}$${price}`);
     }
   }
 
-  // Log the bot in with the DISCORD_TOKEN_ID provided from the .env file
+  // Log the bot in with the 'DISCORD_TOKEN_ID' provided from the '.env' file
   bot.login(process.env.DISCORD_TOKEN_ID);
 }
 
